4d07cb53e1b29224ecc1490018f86179bff6504e
spaceshipofKien <kien99.workspace@gmail.com>
2025-02-26 07:13:16 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
-            keyframes: {
-                slideDown: {
-                    '0%' : {transform: 'translateY(-100%)'},
-                    '100%' : {transform: 'translateY(0)'},
-                }
-            },
-            animation: {
-                slideDown: 'slideDown 1s ease-in-out'
-            }
+            
         },
     },
     plugins: [],
